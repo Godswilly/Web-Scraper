b3531d0a6a32f6701ae174283e7ccec0fb09d3fb
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2020-08-25 08:48:22 +0100

    Add method to parse HTTParty and Nokogiri

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,5 +9,12 @@ class Shoes
     @url = 'https://www.jumia.com.ng/mens-loafers-slip-ons/'
     @shoes = []
     @parsed_page = nil
-  end  
+  end
+
+  private
+
+  def parse_url(url)
+    unparsed_page = HTTParty.get(url)
+    Nokogiri::HTML(unparsed_page)
+  end
 end
